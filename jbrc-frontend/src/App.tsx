28f9f9eb05c0eea424611f43e2b85f8c4b54a063
Josiah Becker <josiah.becker21@gmail.com>
2026-07-14 18:06:45 -0500

    project init (tailwind and shadcn

---
+++ jbrc-frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { Badge } from "./components/ui/badge";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div>
+      hello
+      <Button>click me</Button>
+      <Badge>wowowow</Badge>
+    </div>
+  );
+}
+
+export default App;
